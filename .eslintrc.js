79cb9dbb6c2a3a34e956798dc82ecbc3b9019b26
alehuo <aleksi.huotala@reaktor.com>
2020-09-25 11:28:15 +0300

    Add lint command

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,22 +3,22 @@ module.exports = {
     commonjs: true,
     es6: true,
     node: true,
-    mocha: true
+    mocha: true,
   },
-  extends: ["airbnb-base", "plugin:mocha/recommended"],
-  plugins: ["mocha"],
+  extends: ['airbnb-base', 'plugin:mocha/recommended'],
+  plugins: ['mocha'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaVersion: 2018,
   },
   rules: {
-    "no-underscore-dangle": "off",
-    "func-names": "off",
-    "consistent-return": "off",
-    "mocha/no-mocha-arrows": "off",
-    "mocha/no-setup-in-describe": "off",
+    'no-underscore-dangle': 'off',
+    'func-names': 'off',
+    'consistent-return': 'off',
+    'mocha/no-mocha-arrows': 'off',
+    'mocha/no-setup-in-describe': 'off',
   },
 };
