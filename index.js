378b973bb7e2a3e1eaeb1b6f7486989afed7d30e
alehuo <aleksi.huotala@reaktor.com>
2020-09-25 11:20:23 +0300

    Configure ESLint, remove standard as dependency

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
-var crypto = require('crypto')
-var stream = require('stream')
-var fileType = require('file-type')
-var isSvg = require('is-svg')
-var parallel = require('run-parallel')
+const crypto = require('crypto');
+const stream = require('stream');
+const fileType = require('file-type');
+const isSvg = require('is-svg');
+const parallel = require('run-parallel');
 
-function staticValue (value) {
+function staticValue(value) {
   return function (req, file, cb) {
-    cb(null, value)
-  }
+    cb(null, value);
+  };
 }
 
-var defaultAcl = staticValue('private')
-var defaultContentType = staticValue('application/octet-stream')
+const defaultAcl = staticValue('private');
+const defaultContentType = staticValue('application/octet-stream');
 
-var defaultMetadata = staticValue(null)
-var defaultCacheControl = staticValue(null)
-var defaultContentDisposition = staticValue(null)
-var defaultStorageClass = staticValue('STANDARD')
-var defaultSSE = staticValue(null)
-var defaultSSEKMS = staticValue(null)
+const defaultMetadata = staticValue(null);
+const defaultCacheControl = staticValue(null);
+const defaultContentDisposition = staticValue(null);
+const defaultStorageClass = staticValue('STANDARD');
+const defaultSSE = staticValue(null);
+const defaultSSEKMS = staticValue(null);
 
-function defaultKey (req, file, cb) {
-  crypto.randomBytes(16, function (err, raw) {
-    cb(err, err ? undefined : raw.toString('hex'))
-  })
+function defaultKey(req, file, cb) {
+  crypto.randomBytes(16, (err, raw) => {
+    cb(err, err ? undefined : raw.toString('hex'));
+  });
 }
 
-function autoContentType (req, file, cb) {
-  file.stream.once('data', function (firstChunk) {
-    fileType.fromBuffer(firstChunk).then(type => {
-      let mime = null
+function autoContentType(req, file, cb) {
+  file.stream.once('data', (firstChunk) => {
+    fileType.fromBuffer(firstChunk).then((type) => {
+      let mime = null;
       if (type) {
-        mime = type.mime
+        mime = type.mime;
       } else if (isSvg(firstChunk)) {
-        mime = 'image/svg+xml'
+        mime = 'image/svg+xml';
       } else {
-        mime = 'application/octet-stream'
+        mime = 'application/octet-stream';
       }
-      var outStream = new stream.PassThrough()
-      outStream.write(firstChunk)
-      file.stream.pipe(outStream)
-      cb(null, mime, outStream)
-    }).catch(err => cb(err))
-  })
+      const outStream = new stream.PassThrough();
+      outStream.write(firstChunk);
+      file.stream.pipe(outStream);
+      cb(null, mime, outStream);
+    }).catch((err) => cb(err));
+  });
 }
 
-function collect (storage, req, file, cb) {
+function collect(storage, req, file, cb) {
   parallel([
     storage.getBucket.bind(storage, req, file),
     storage.getKey.bind(storage, req, file),
@@ -55,12 +55,12 @@ function collect (storage, req, file, cb) {
     storage.getContentDisposition.bind(storage, req, file),
     storage.getStorageClass.bind(storage, req, file),
     storage.getSSE.bind(storage, req, file),
-    storage.getSSEKMS.bind(storage, req, file)
-  ], function (err, values) {
-    if (err) return cb(err)
+    storage.getSSEKMS.bind(storage, req, file),
+  ], (err, values) => {
+    if (err) return cb(err);
 
-    storage.getContentType(req, file, function (err, contentType, replacementStream) {
-      if (err) return cb(err)
+    storage.getContentType(req, file, (error, contentType, replacementStream) => {
+      if (error) return cb(error);
 
       cb.call(storage, null, {
         bucket: values[0],
@@ -70,96 +70,96 @@ function collect (storage, req, file, cb) {
         cacheControl: values[4],
         contentDisposition: values[5],
         storageClass: values[6],
-        contentType: contentType,
-        replacementStream: replacementStream,
+        contentType,
+        replacementStream,
         serverSideEncryption: values[7],
-        sseKmsKeyId: values[8]
-      })
-    })
-  })
+        sseKmsKeyId: values[8],
+      });
+    });
+  });
 }
 
-function S3Storage (opts) {
+function S3Storage(opts) {
   switch (typeof opts.s3) {
-    case 'object': this.s3 = opts.s3; break
-    default: throw new TypeError('Expected opts.s3 to be object')
+    case 'object': this.s3 = opts.s3; break;
+    default: throw new TypeError('Expected opts.s3 to be object');
   }
 
   switch (typeof opts.bucket) {
-    case 'function': this.getBucket = opts.bucket; break
-    case 'string': this.getBucket = staticValue(opts.bucket); break
-    case 'undefined': throw new Error('bucket is required')
-    default: throw new TypeError('Expected opts.bucket to be undefined, string or function')
+    case 'function': this.getBucket = opts.bucket; break;
+    case 'string': this.getBucket = staticValue(opts.bucket); break;
+    case 'undefined': throw new Error('bucket is required');
+    default: throw new TypeError('Expected opts.bucket to be undefined, string or function');
   }
 
   switch (typeof opts.key) {
-    case 'function': this.getKey = opts.key; break
-    case 'undefined': this.getKey = defaultKey; break
-    default: throw new TypeError('Expected opts.key to be undefined or function')
+    case 'function': this.getKey = opts.key; break;
+    case 'undefined': this.getKey = defaultKey; break;
+    default: throw new TypeError('Expected opts.key to be undefined or function');
   }
 
   switch (typeof opts.acl) {
-    case 'function': this.getAcl = opts.acl; break
-    case 'string': this.getAcl = staticValue(opts.acl); break
-    case 'undefined': this.getAcl = defaultAcl; break
-    default: throw new TypeError('Expected opts.acl to be undefined, string or function')
+    case 'function': this.getAcl = opts.acl; break;
+    case 'string': this.getAcl = staticValue(opts.acl); break;
+    case 'undefined': this.getAcl = defaultAcl; break;
+    default: throw new TypeError('Expected opts.acl to be undefined, string or function');
   }
 
   switch (typeof opts.contentType) {
-    case 'function': this.getContentType = opts.contentType; break
-    case 'undefined': this.getContentType = defaultContentType; break
-    default: throw new TypeError('Expected opts.contentType to be undefined or function')
+    case 'function': this.getContentType = opts.contentType; break;
+    case 'undefined': this.getContentType = defaultContentType; break;
+    default: throw new TypeError('Expected opts.contentType to be undefined or function');
   }
 
   switch (typeof opts.metadata) {
-    case 'function': this.getMetadata = opts.metadata; break
-    case 'undefined': this.getMetadata = defaultMetadata; break
-    default: throw new TypeError('Expected opts.metadata to be undefined or function')
+    case 'function': this.getMetadata = opts.metadata; break;
+    case 'undefined': this.getMetadata = defaultMetadata; break;
+    default: throw new TypeError('Expected opts.metadata to be undefined or function');
   }
 
   switch (typeof opts.cacheControl) {
-    case 'function': this.getCacheControl = opts.cacheControl; break
-    case 'string': this.getCacheControl = staticValue(opts.cacheControl); break
-    case 'undefined': this.getCacheControl = defaultCacheControl; break
-    default: throw new TypeError('Expected opts.cacheControl to be undefined, string or function')
+    case 'function': this.getCacheControl = opts.cacheControl; break;
+    case 'string': this.getCacheControl = staticValue(opts.cacheControl); break;
+    case 'undefined': this.getCacheControl = defaultCacheControl; break;
+    default: throw new TypeError('Expected opts.cacheControl to be undefined, string or function');
   }
 
   switch (typeof opts.contentDisposition) {
-    case 'function': this.getContentDisposition = opts.contentDisposition; break
-    case 'string': this.getContentDisposition = staticValue(opts.contentDisposition); break
-    case 'undefined': this.getContentDisposition = defaultContentDisposition; break
-    default: throw new TypeError('Expected opts.contentDisposition to be undefined, string or function')
+    case 'function': this.getContentDisposition = opts.contentDisposition; break;
+    case 'string': this.getContentDisposition = staticValue(opts.contentDisposition); break;
+    case 'undefined': this.getContentDisposition = defaultContentDisposition; break;
+    default: throw new TypeError('Expected opts.contentDisposition to be undefined, string or function');
   }
 
   switch (typeof opts.storageClass) {
-    case 'function': this.getStorageClass = opts.storageClass; break
-    case 'string': this.getStorageClass = staticValue(opts.storageClass); break
-    case 'undefined': this.getStorageClass = defaultStorageClass; break
-    default: throw new TypeError('Expected opts.storageClass to be undefined, string or function')
+    case 'function': this.getStorageClass = opts.storageClass; break;
+    case 'string': this.getStorageClass = staticValue(opts.storageClass); break;
+    case 'undefined': this.getStorageClass = defaultStorageClass; break;
+    default: throw new TypeError('Expected opts.storageClass to be undefined, string or function');
   }
 
   switch (typeof opts.serverSideEncryption) {
-    case 'function': this.getSSE = opts.serverSideEncryption; break
-    case 'string': this.getSSE = staticValue(opts.serverSideEncryption); break
-    case 'undefined': this.getSSE = defaultSSE; break
-    default: throw new TypeError('Expected opts.serverSideEncryption to be undefined, string or function')
+    case 'function': this.getSSE = opts.serverSideEncryption; break;
+    case 'string': this.getSSE = staticValue(opts.serverSideEncryption); break;
+    case 'undefined': this.getSSE = defaultSSE; break;
+    default: throw new TypeError('Expected opts.serverSideEncryption to be undefined, string or function');
   }
 
   switch (typeof opts.sseKmsKeyId) {
-    case 'function': this.getSSEKMS = opts.sseKmsKeyId; break
-    case 'string': this.getSSEKMS = staticValue(opts.sseKmsKeyId); break
-    case 'undefined': this.getSSEKMS = defaultSSEKMS; break
-    default: throw new TypeError('Expected opts.sseKmsKeyId to be undefined, string, or function')
+    case 'function': this.getSSEKMS = opts.sseKmsKeyId; break;
+    case 'string': this.getSSEKMS = staticValue(opts.sseKmsKeyId); break;
+    case 'undefined': this.getSSEKMS = defaultSSEKMS; break;
+    default: throw new TypeError('Expected opts.sseKmsKeyId to be undefined, string, or function');
   }
 }
 
 S3Storage.prototype._handleFile = function (req, file, cb) {
   collect(this, req, file, function (err, opts) {
-    if (err) return cb(err)
+    if (err) return cb(err);
 
-    var currentSize = 0
+    let currentSize = 0;
 
-    var params = {
+    const params = {
       Bucket: opts.bucket,
       Key: opts.key,
       ACL: opts.acl,
@@ -169,21 +169,21 @@ S3Storage.prototype._handleFile = function (req, file, cb) {
       StorageClass: opts.storageClass,
       ServerSideEncryption: opts.serverSideEncryption,
       SSEKMSKeyId: opts.sseKmsKeyId,
-      Body: (opts.replacementStream || file.stream)
-    }
+      Body: (opts.replacementStream || file.stream),
+    };
 
     if (opts.contentDisposition) {
-      params.ContentDisposition = opts.contentDisposition
+      params.ContentDisposition = opts.contentDisposition;
     }
 
-    var upload = this.s3.upload(params)
+    const upload = this.s3.upload(params);
 
-    upload.on('httpUploadProgress', function (ev) {
-      if (ev.total) currentSize = ev.total
-    })
+    upload.on('httpUploadProgress', (ev) => {
+      if (ev.total) currentSize = ev.total;
+    });
 
-    upload.send(function (err, result) {
-      if (err) return cb(err)
+    upload.send((error, result) => {
+      if (error) return cb(error);
 
       cb(null, {
         size: currentSize,
@@ -197,19 +197,19 @@ S3Storage.prototype._handleFile = function (req, file, cb) {
         metadata: opts.metadata,
         location: result.Location,
         etag: result.ETag,
-        versionId: result.VersionId
-      })
-    })
-  })
-}
+        versionId: result.VersionId,
+      });
+    });
+  });
+};
 
 S3Storage.prototype._removeFile = function (req, file, cb) {
-  this.s3.deleteObject({ Bucket: file.bucket, Key: file.key }, cb)
-}
+  this.s3.deleteObject({ Bucket: file.bucket, Key: file.key }, cb);
+};
 
 module.exports = function (opts) {
-  return new S3Storage(opts)
-}
+  return new S3Storage(opts);
+};
 
-module.exports.AUTO_CONTENT_TYPE = autoContentType
-module.exports.DEFAULT_CONTENT_TYPE = defaultContentType
+module.exports.AUTO_CONTENT_TYPE = autoContentType;
+module.exports.DEFAULT_CONTENT_TYPE = defaultContentType;
